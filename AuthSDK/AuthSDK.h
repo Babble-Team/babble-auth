5d802d73b4de3aa5e691d395403334908ebced66
Chris M <cph.lowiec@gmail.com>
2022-03-12 19:29:06 +0100

    setup basic configuration

---
+++ AuthSDK/AuthSDK.h
@@ -0,0 +1,18 @@
+//
+//  AuthSDK.h
+//  AuthSDK
+//
+//  Created by Krzysztof Łowiec on 12/03/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bubble_auth.
+FOUNDATION_EXPORT double bubble_authVersionNumber;
+
+//! Project version string for bubble_auth.
+FOUNDATION_EXPORT const unsigned char bubble_authVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bubble_auth/PublicHeader.h>
+
+
